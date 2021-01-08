aa8bfa77ab0a593df62b16d7d3c035ff1693823d
aaiilee0715 <achansylee@outlook.com>
2021-01-08 14:39:46 -0800

    Counter for cpu is working

--- js/main.js
+++ js/main.js
@@ -18,51 +18,101 @@ let bestOf7Btn = document.getElementById('bestOf7Btn');
 
 //Cpu------------
 let url = 'https://csa2020studentapi.azurewebsites.net/rpsls';
-let computerChoice; //whatever string response text return
+let computerChoice; //Will store whatever string response text return
 let selectionChoices;
 let roundOption = 0;
 
+
+//pvp
 playerVsPlayerBtn.addEventListener('click', function () {
     alert('Lets have a good game!');
-    // console.log('Lets have a good game!');
 });
+
+//user vs cpu
 playerVsCPUBtn.addEventListener('click', function () {
     alert("Computer accept human challenge...");
-    // console.log('Computer accept human challenge...');
     fetch(url).then(data => { //fetch the url, grap the response in text
         // console.log(data);
         data.text().then( //data.text will only grab the string
             function (value) {
-                console.log(value); //will show the url value in console
+                // console.log(value); //will show the url value in console
                 computerChoice = value; //cpu choice will equall to whatever value is fetch
                 playRound();
             }
         )
     })
+    document.getElementById('playerVsCPUBtn').addEventListener('click', function () {
+        document.getElementById('mainCont').style.display = 'none';
+        document.getElementById('gameRulesPage').style.display = 'none';
+        document.getElementById('rulesCont').style.display = "none";
+        document.getElementById('playerMode').style.display = "flex";
+        document.getElementById('roundOption').style.display = "flex";
+
+    });
+
 });
+
 
 function playRound() {
     document.getElementById('roundOption').style.display = "flex";
-
-    oneRoundBtn.addEventListener('click', function () {
+    //1 win------
+    document.getElementById('oneRoundBtn').addEventListener('click', function () {
         alert('You no fun');
         roundOption = 1;
+        document.getElementById('mainCont').style.display = 'none';
+        document.getElementById('gameRulesPage').style.display = 'none';
+        document.getElementById('rulesCont').style.display = "flex";
+        document.getElementById('playerMode').style.display = "none";
+        document.getElementById('roundOption').style.display = "none";
+        document.getElementById('userChoice').style.display = "flex";
+        document.getElementById('resultScore').style.display = 'none';
+        compareRound(computerChoice, selectionChoices);
+        computerChoice = computerChoice.toLowerCase();
+        selectionChoices = selectionChoices.toLowerCase();
+        document.getElementById('playerChoice').innerText = selectionChoices;
+        document.getElementById('computerChoice').innerText = computerChoice;
+
     });
+    // 3 out of 5
     bestOf5Btn.addEventListener('click', function () {
         alert('Who will get 3 first?');
         roundOption = 5;
+        document.getElementById('mainCont').style.display = 'none';
+        document.getElementById('gameRulesPage').style.display = 'none';
+        document.getElementById('rulesCont').style.display = "flex";
+        document.getElementById('playerMode').style.display = "none";
+        document.getElementById('roundOption').style.display = "none";
+        document.getElementById('best5Choice').style.display = "flex";
+        document.getElementById('resultScore').style.display = 'none';
+        compareRound(computerChoice, selectionChoices);
+        computerChoice = computerChoice.toLowerCase();
+        selectionChoices = selectionChoices.toLowerCase();
+        document.getElementById('playerChoice').innerText = selectionChoices;
+        document.getElementById('computerChoice').innerText = computerChoice;
     });
+    //4 out of 7
     bestOf7Btn.addEventListener('click', function () {
         alert('You must be bored...Lets-a go!');
         roundOption = 7;
+        document.getElementById('mainCont').style.display = 'none';
+        document.getElementById('gameRulesPage').style.display = 'none';
+        document.getElementById('rulesCont').style.display = "flex";
+        document.getElementById('playerMode').style.display = "none";
+        document.getElementById('roundOption').style.display = "none";
+        document.getElementById('best4Choice').style.display = "flex";
+        document.getElementById('resultScore').style.display = 'none';
+        compareRound(computerChoice, selectionChoices);
+        computerChoice = computerChoice.toLowerCase();
+        selectionChoices = selectionChoices.toLowerCase();
+        document.getElementById('playerChoice').innerText = selectionChoices;
+        document.getElementById('computerChoice').innerText = computerChoice;
     });
+
 }
+// console.log(fetch("https://csa2020studentapi.azurewebsites.net/rpsls")); //show on console side what value is return
 
 
-console.log(fetch("https://csa2020studentapi.azurewebsites.net/rpsls")); //show on console side what value is return
 
-
-// function outCome() {
 document.getElementById('rock').addEventListener('click', function () {
     selectionChoices = "rock";
     compareRound(computerChoice, selectionChoices);
@@ -83,18 +133,23 @@ document.getElementById('spock').addEventListener('click', function () {
     selectionChoices = "spock";
     compareRound(computerChoice, selectionChoices);
 })
-// }
-// console.log(outCome);
-//create a function for variable computer game
+
+
+let userScore = 0;
+let cpuScore = 0;
+
+
+let results = document.getElementById('results');
+
 function compareRound(computerChoice, selectionChoices) {
-    console.log("Get Winner: player", selectionChoices, "computer", computerChoice);
     if (computerChoice == selectionChoices) {
         document.getElementById('userChoice').style.display = 'none';
         document.getElementById('resultScore').style.display = 'flex';
-        document.getElementById('playerChoice').innerText = selectionChoices;
+        document.getElementById('results').style.display = 'flex';
+         document.getElementById('computerChoice').innerText = computerChoice;
         document.getElementById('computerChoice').innerText = computerChoice;
-        document.getElementById('draw').innerText = 'draw';
-        return (computerChoice, selectionChoices);
+        results.innerText = 'draw';
+
     }
     else if (selectionChoices == "rock") {
         if (computerChoice == "paper") {
@@ -102,7 +157,9 @@ function compareRound(computerChoice, selectionChoices) {
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-            
+            results.innerText = 'Computer Win';
+            cpuScore ++;
+            document.getElementById('cpuScore').innerText = cpuScore;
 
         }
         else if (computerChoice == "scissors") {
@@ -110,21 +167,25 @@ function compareRound(computerChoice, selectionChoices) {
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-            
+            results.innerText = 'You Win';
+            userScore ++;
+            document.getElementById('userScore').innerText = userScore;
         }
         else if (computerChoice == "lizard") {
             document.getElementById('resultScore').style.display = 'flex';
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'Computer Win';
+            cpuScore ++;
+            document.getElementById('cpuScore').innerText = cpuScore;
         }
         else {
-            document.getElementById('resultScore').style.display = 'flex';
-            document.getElementById('userChoice').style.display = 'none';
-            document.getElementById('playerChoice').innerText = selectionChoices;
-            document.getElementById('computerChoice').innerText = computerChoice;
-
+            // document.getElementById('resultScore').style.display = 'flex';
+            // document.getElementById('userChoice').style.display = 'none';
+            // document.getElementById('playerChoice').innerText = selectionChoices;
+            // document.getElementById('computerChoice').innerText = computerChoice;
+            // document.getElementById('cpuScore').innerText = cpuScore;
         }
     }
     else if (selectionChoices == "paper") {
@@ -133,7 +194,9 @@ function compareRound(computerChoice, selectionChoices) {
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'You Win';
+            userScore ++;
+            document.getElementById('userScore').innerText = userScore;
         }
 
         else if (computerChoice == "scissors") {
@@ -141,21 +204,25 @@ function compareRound(computerChoice, selectionChoices) {
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'Computer Win';
+               cpuScore ++;
+               document.getElementById('cpuScore').innerText = cpuScore;
         }
         else if (computerChoice == "lizard") {
             document.getElementById('resultScore').style.display = 'flex';
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'Computer Win';
+               cpuScore ++;
+               document.getElementById('cpuScore').innerText = cpuScore;
         }
         else {
-            document.getElementById('resultScore').style.display = 'flex';
-            document.getElementById('userChoice').style.display = 'none';
-            document.getElementById('playerChoice').innerText = selectionChoices;
-            document.getElementById('computerChoice').innerText = computerChoice;
-
+            // document.getElementById('resultScore').style.display = 'flex';
+            // document.getElementById('userChoice').style.display = 'none';
+            // document.getElementById('playerChoice').innerText = selectionChoices;
+            // document.getElementById('computerChoice').innerText = computerChoice;
+          
         }
     }
     else if (selectionChoices == "scissors") {
@@ -164,27 +231,33 @@ function compareRound(computerChoice, selectionChoices) {
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'Computer Win';
+            cpuScore ++;
+            document.getElementById('cpuScore').innerText = cpuScore;
         }
         else if (computerChoice == "paper") {
             document.getElementById('resultScore').style.display = 'flex';
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'You Win';
+               userScore ++;
+               document.getElementById('userScore').innerText = userScore;
         }
         else if (computerChoice == "lizard") {
             document.getElementById('resultScore').style.display = 'flex';
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'You Win';
+               userScore ++;
+               document.getElementById('userScore').innerText = userScore;
         }
         else {
-            document.getElementById('resultScore').style.display = 'flex';
-            document.getElementById('userChoice').style.display = 'none';
-            document.getElementById('playerChoice').innerText = selectionChoices;
-            document.getElementById('computerChoice').innerText = computerChoice;
+            // document.getElementById('resultScore').style.display = 'flex';
+            // document.getElementById('userChoice').style.display = 'none';
+            // document.getElementById('playerChoice').innerText = selectionChoices;
+            // document.getElementById('computerChoice').innerText = computerChoice;
 
         }
     }
@@ -194,27 +267,33 @@ function compareRound(computerChoice, selectionChoices) {
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'You Win';
+            userScore ++;
+            document.getElementById('userScore').innerText = userScore;
         }
         else if (computerChoice == "paper") {
             document.getElementById('resultScore').style.display = 'flex';
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'You Win';
+            userScore ++;
+            document.getElementById('userScore').innerText = userScore;
         }
         else if (computerChoice == "scissors") {
             document.getElementById('resultScore').style.display = 'flex';
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'Computer Win';
+            cpuScore ++;
+            document.getElementById('cpuScore').innerText = cpuScore;
         }
         else {
-            document.getElementById('resultScore').style.display = 'flex';
-            document.getElementById('userChoice').style.display = 'none';
-            document.getElementById('playerChoice').innerText = selectionChoices;
-            document.getElementById('computerChoice').innerText = computerChoice;
+            // document.getElementById('resultScore').style.display = 'flex';
+            // document.getElementById('userChoice').style.display = 'none';
+            // document.getElementById('playerChoice').innerText = selectionChoices;
+            // document.getElementById('computerChoice').innerText = computerChoice;
 
         }
     }
@@ -224,33 +303,44 @@ function compareRound(computerChoice, selectionChoices) {
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'Computer Win';
+            cpuScore ++;
+            document.getElementById('cpuScore').innerText = cpuScore;
         }
         else if (computerChoice == "paper") {
             document.getElementById('resultScore').style.display = 'flex';
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'Computer Win';
+            cpuScore ++;
+            document.getElementById('cpuScore').innerText = cpuScore;
         }
         else if (computerChoice == "scissors") {
             document.getElementById('resultScore').style.display = 'flex';
             document.getElementById('userChoice').style.display = 'none';
             document.getElementById('playerChoice').innerText = selectionChoices;
             document.getElementById('computerChoice').innerText = computerChoice;
-
+            results.innerText = 'You Win';
+            cpuScore ++;
+            document.getElementById('cpuScore').innerText = cpuScore;
         }
     }
-
-
+    
 }
-// console.log(compareRound);
+
+
+
+
 
 //create an event listener for each button to toggle between pages
 document.getElementById('rules').addEventListener('click', function () {
     document.getElementById('mainCont').style.display = 'none';
     document.getElementById('gameRulesPage').style.display = 'flex';
     document.getElementById('rulesCont').style.display = "none";
+    document.getElementById('best5Choice').style.display = "none";
+    document.getElementById('best4Choice').style.display = "none";
+
 });
 document.getElementById('readyToPlay').addEventListener('click', function () {
     document.getElementById('mainCont').style.display = 'none';
@@ -258,6 +348,7 @@ document.getElementById('readyToPlay').addEventListener('click', function () {
     document.getElementById('rulesCont').style.display = "none";
     document.getElementById('playerMode').style.display = "flex";
     document.getElementById('roundOption').style.display = "none";
+
 });
 document.getElementById('rulesPlayBtn').addEventListener('click', function () {
     document.getElementById('mainCont').style.display = 'none';
@@ -265,23 +356,6 @@ document.getElementById('rulesPlayBtn').addEventListener('click', function () {
     document.getElementById('rulesCont').style.display = "none";
     document.getElementById('playerMode').style.display = "flex";
     document.getElementById('roundOption').style.display = "none";
-});
-document.getElementById('playerVsCPUBtn').addEventListener('click', function () {
-    document.getElementById('mainCont').style.display = 'none';
-    document.getElementById('gameRulesPage').style.display = 'none';
-    document.getElementById('rulesCont').style.display = "none";
-    document.getElementById('playerMode').style.display = "none";
-    document.getElementById('roundOption').style.display = "flex";
-    // document.getElementById('scores').style.display = "none";
-});
-document.getElementById('oneRoundBtn').addEventListener('click', function () {
-    document.getElementById('mainCont').style.display = 'none';
-    document.getElementById('gameRulesPage').style.display = 'none';
-    document.getElementById('rulesCont').style.display = "none";
-    document.getElementById('playerMode').style.display = "none";
-    document.getElementById('roundOption').style.display = "none";
-    document.getElementById('userChoice').style.display = "flex";
-    // document.getElementById('scores').style.display = "flex";
 });
 
 
